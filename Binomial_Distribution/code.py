1ad2db0fbbedfd59a05d6cfa4e533d30f04aa9c9
Vinicius Zambotti <zambottivinicius@gmail.com>
2019-11-05 16:06:33 -0300

    comment fix

--- Binomial_Distribution/code.py
+++ Binomial_Distribution/code.py
@@ -2,7 +2,7 @@
 Task
 The ratio of boys to girls for babies born in Russia is 1.09: 1 .
 If there is 1 child born per birth, what proportion of Russian families with exactly 6 children will have at least 3 boys?
-Write a program to compute the answer using the above parameters. Then print your result, rounded to a scale of  decimal places (i.e.,  format).
+Write a program to compute the answer using the above parameters. Then print your result, rounded to a scale of 3 decimal places (i.e.,1.234 format).
 '''
 
 import math
