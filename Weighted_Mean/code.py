dc54682d5df533ea822d00f9c2e20ec937c3103a
Vinicius Zambotti <zambottivinicius@gmail.com>
2019-11-05 15:32:46 -0300

    Fixing comments

--- Weighted_Mean/code.py
+++ Weighted_Mean/code.py
@@ -1,5 +1,7 @@
 '''
-Given an array,X, ofN integers and an array,W , representing the respective weights of X's elements, calculate and print the weighted mean of X's elements. Your answer should be rounded to a scale of 1 decimal place (i.e.,12.3 format).
+Given an array,X, ofN integers and an array,W , representing the respective weights of X's elements, 
+calculate and print the weighted mean of X's elements. Your answer should be rounded to a scale of 1 
+decimal place (i.e.,12.3 format).
 '''
 
 x = [10,40,30,50,20]
